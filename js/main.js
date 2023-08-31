d5e31f979acaf671d2eda16b66d780e3ce657d90
Almuheetu Shihab <almuheetu@gmail.com>
2023-08-31 21:41:58 +0600

    Blog section finished

--- js/main.js
+++ js/main.js
@@ -11,5 +11,12 @@ $(document).ready(function(){
 
 
     // owl-carousel for blog
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 2000,
+        dots: false,
+        nav: true,
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+    });
 });
